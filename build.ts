e6213bdcd7888d561be6b10d2123ffeb7fd15c87
lesh <lesh@sysphere.org>
2025-01-18 21:54:37 +0800

    just some progress bars

--- build.ts
+++ build.ts
@@ -1,18 +1,41 @@
-import { green } from "https://deno.land/std@0.224.0/fmt/colors.ts";
+import { green, blue } from "https://deno.land/std@0.224.0/fmt/colors.ts";
 import { parse } from "https://deno.land/std@0.224.0/flags/mod.ts";
 import { walk } from "https://deno.land/std@0.224.0/fs/mod.ts";
 import * as path from "https://deno.land/std@0.224.0/path/mod.ts";
 import PQueue from "https://deno.land/x/p_queue@1.0.1/mod.ts";
+import { MultiProgressBar } from "https://deno.land/x/progress@v1.3.9/mod.ts";
 
 const queue = new PQueue({
-    concurrency: 10,
+    concurrency: 3,
 });
 
 const SRC_DIR = "site";
 const DEST_DIR = "site";
 
-async function buildFile(file: string) {
-    console.log(green("build"), file);
+const progress = new MultiProgressBar({
+    title: "build",
+    // clear: true,
+    complete: green("OK"),
+    width: 1,
+    incomplete: blue("processing"),
+    display: ":text :bar",
+});
+
+const bars = [];
+
+async function buildFile(file: string, usebar: bool = false) {
+    const bar = {
+        completed: 0,
+        total: 100,
+        text: file.padEnd(50),
+    };
+    if (usebar) {
+        bars.push(bar);
+        await progress.render(bars);
+    }
+
+    //console.log(green("build"), file);
+
     const destPath = file.replace(SRC_DIR, DEST_DIR).replace(".org", ".html");
     await Deno.mkdir(path.dirname(destPath), { recursive: true });
     const templatePath = path.join(Deno.cwd(), "template.html");
@@ -44,16 +67,22 @@ async function buildFile(file: string) {
                 decoder.decode(output.stderr),
         );
     }
+
+    if (usebar) {
+        bar.completed = 100;
+        await progress.render(bars);
+    } else {
+        console.log("\n" + green("build"), file);
+    }
 }
 
 async function buildAll() {
     for await (const entry of walk(SRC_DIR, { exts: [".org"] })) {
-        queue.add(() => buildFile(entry.path));
+        queue.add(() => buildFile(entry.path, true));
     }
 }
 
 async function watch() {
-    console.log(green("watching"));
     const watcher = Deno.watchFs(SRC_DIR);
     for await (const event of watcher) {
         for (const file of event.paths) {
